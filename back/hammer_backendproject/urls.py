2c3d68aa4d03f786568661a6e97b40aaa31ee2b0
Kai Swain <kaiferseain@icloud.com>
2025-09-04 12:43:38 -0500

    Restore original PDF generation using PyMuPDF instead of WeasyPrint temporary bypass

--- back/hammer_backendproject/urls.py
+++ back/hammer_backendproject/urls.py
@@ -6,9 +6,8 @@ from django.conf.urls.static import static
 from rest_framework import routers
 from hammer_backendapi.views import login_user, StudentForeignKeyOptionsView
 from hammer_backendapi.views.students import StudentViewSet
-# Temporarily use simplified certificate views
-from hammer_backendapi.views.certificates_temp import generate_workforce_certificate,generate_employability_certificate,generate_hammermath_certificate,generate_nccer_certificate,generate_osha_certificate,generate_portfolio_certificate
-from hammer_backendapi.views.generate_all_temp import generate_all_certificates
+# Import original certificate views
+from hammer_backendapi.views import certificates, generate_all
 from hammer_backendapi.views.health import health_check, api_info
 from hammer_backendapi.views.support import support_request
 
@@ -22,13 +21,13 @@ router.register(r"students", StudentViewSet, "student")
 api_patterns = [
     path('', include(router.urls)),
     path('login/', login_user),
-    path("generate/all/", generate_all_certificates),
-    path("generate/portfolio/", generate_portfolio_certificate),
-    path("generate/nccer/", generate_nccer_certificate),
-    path("generate/osha/", generate_osha_certificate),
-    path("generate/hammermath/", generate_hammermath_certificate),
-    path("generate/employability/", generate_employability_certificate),
-    path("generate/workforce/", generate_workforce_certificate),
+    path("generate/all/", generate_all.generate_all_certificates),
+    path("generate/portfolio/", certificates.generate_portfolio_certificate),
+    path("generate/nccer/", certificates.generate_nccer_certificate),
+    path("generate/osha/", certificates.generate_osha_certificate),
+    path("generate/hammermath/", certificates.generate_hammermath_certificate),
+    path("generate/employability/", certificates.generate_employability_certificate),
+    path("generate/workforce/", certificates.generate_workforce_certificate),
     path("details/", StudentForeignKeyOptionsView.as_view()),
     path("health/", health_check),
     path("info/", api_info),
